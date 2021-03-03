e0bf131ac5b839aa5d3b8a6e4fa2a6b911ceeae3
ronggggg <79974156+ronggggg@users.noreply.github.com>
2021-03-03 18:25:17 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+''' main '''
+# 使用自建的套件 myMath 來計算使用者輸入的五個數的平均值
+import myMath.myStatistics
+number = []
+for i in range(5):
+     number.append(int(input(":")))
+
+print(number)
+print(myMath.myStatistics.myMean(number))
